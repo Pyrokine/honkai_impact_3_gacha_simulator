664603f0404b1002140d26affd524be53034f6b7
pyrokine <pyrokinecist@aliyun.com>
2022-06-16 22:15:02 -0400

    update

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 两个卡池均使用许愿机制且均抽齐四件套
 
+新增新卡池，继承精准的概率与扩充的保底机制
+
 https://www.cnblogs.com/Pyrokine/p/16351324.html
--- multi_thread.py
+++ multi_thread.py
@@ -15,6 +15,9 @@ focus_regular_4_star_exclude_up_rate = 0.12395 / publicity_rate_to_single_pull_r
 expansion_regular_weapon_rate = 0.01831 / publicity_rate_to_single_pull_rate
 expansion_regular_stigmata_rate = 0.00916 / publicity_rate_to_single_pull_rate
 expansion_regular_4_star_exclude_up_rate = 0.12395 / publicity_rate_to_single_pull_rate - expansion_regular_weapon_rate - expansion_regular_stigmata_rate * 3.0
+new_regular_weapon_rate = 0.02479 / publicity_rate_to_single_pull_rate
+new_regular_stigmata_rate = 0.01240 / publicity_rate_to_single_pull_rate
+new_regular_4_star_exclude_up_rate = 0.12395 / publicity_rate_to_single_pull_rate - new_regular_weapon_rate - new_regular_stigmata_rate * 3.0
 
 guarantee_weapon_rate = 0.2
 guarantee_stigmata_rate = 0.1
@@ -22,25 +25,27 @@ guarantee_4_star_exclude_up_rate = 1 - guarantee_weapon_rate - guarantee_stigmat
 guarantee_small = 10
 guarantee_big = 50
 
-# banner_sample_size 为浮点随机数向整数映射时整数的范围，该值越大则保留小数点越多，但耗时则越长
-# gacha_sample_size * thread_pool_size 和运行时占用内存正相关，两者乘积在 1e7 时会占用 11.5G 的内存，可以根据实际情况调整
-# gacha_sample_size * thread_number 为总共进行的采样次数
-banner_sample_size = int(1e4)
-gacha_sample_size = int(1e7)
-thread_pool_size = int(3)
-thread_number = int(12)
+sample_accuracy = int(1e4)
+banner_size = int(1e6)
+pnt_threshold = banner_size - int(2e3)
+gacha_sample_size = int(1e8)
+x_truncate = 200
 
-# -2 非四星装备  -1 四星非UP装备  0 UP武器  1 UP圣痕上  2 UP圣痕中  3 UP圣痕下
-banner_guarantee, banner_regular_focus, banner_regular_expansion = {}, {}, {}
+thread_pool_size = int(10)
+thread_number = int(10)
+preserved_size = 1500
+
+guarantee_banner, focus_regular_banner, expansion_regular_banner, new_regular_banner = {}, {}, {}, {}
 
 
 # %%
 def generate_banner():
     pnt, val = 0, -1
-    for i in range(banner_sample_size):
-        banner_guarantee.update({i: -2})
-        banner_regular_focus.update({i: -2})
-        banner_regular_expansion.update({i: -2})
+    for i in range(sample_accuracy):
+        guarantee_banner.update({i: -2})
+        focus_regular_banner.update({i: -2})
+        expansion_regular_banner.update({i: -2})
+        new_regular_banner.update({i: -2})
 
     for i in np.array(
             [
@@ -49,9 +54,9 @@ def generate_banner():
                 guarantee_stigmata_rate,
                 guarantee_stigmata_rate,
                 guarantee_stigmata_rate
-            ]) * banner_sample_size:
+            ]) * sample_accuracy:
         for j in range(int(i)):
-            banner_guarantee[pnt] = val
+            guarantee_banner[pnt] = val
             pnt += 1
         val += 1
 
@@ -63,9 +68,9 @@ def generate_banner():
                 focus_regular_stigmata_rate,
                 focus_regular_stigmata_rate,
                 focus_regular_stigmata_rate
-            ]) * banner_sample_size:
+            ]) * sample_accuracy:
         for j in range(int(i)):
-            banner_regular_focus[pnt] = val
+            focus_regular_banner[pnt] = val
             pnt += 1
         val += 1
 
@@ -77,9 +82,23 @@ def generate_banner():
                 expansion_regular_stigmata_rate,
                 expansion_regular_stigmata_rate,
                 expansion_regular_stigmata_rate
-            ]) * banner_sample_size:
+            ]) * sample_accuracy:
         for j in range(int(i)):
-            banner_regular_expansion[pnt] = val
+            expansion_regular_banner[pnt] = val
+            pnt += 1
+        val += 1
+
+    pnt, val = 0, -1
+    for i in np.array(
+            [
+                new_regular_4_star_exclude_up_rate,
+                new_regular_weapon_rate,
+                new_regular_stigmata_rate,
+                new_regular_stigmata_rate,
+                new_regular_stigmata_rate
+            ]) * sample_accuracy:
+        for j in range(int(i)):
+            new_regular_banner[pnt] = val
             pnt += 1
         val += 1
 
@@ -97,9 +116,9 @@ class Gacha_Focus:
         self.total_pulling = 0
 
     def single_pull(self):
-        result = banner_guarantee[self.args['random_pool'][self.args['pnt_random_pool']]] \
+        result = guarantee_banner[self.args['random_pool'][self.args['pnt_random_pool']]] \
             if self.pity_counter_small >= guarantee_small \
-            else banner_regular_focus[self.args['random_pool'][self.args['pnt_random_pool']]]
+            else focus_regular_banner[self.args['random_pool'][self.args['pnt_random_pool']]]
         self.args['pnt_random_pool'] += 1
 
         if result >= -1:
@@ -146,9 +165,9 @@ class Gacha_Expansion:
 
             return True
         else:
-            result = banner_guarantee[self.args['random_pool'][self.args['pnt_random_pool']]] \
+            result = guarantee_banner[self.args['random_pool'][self.args['pnt_random_pool']]] \
                 if self.pity_counter_small >= guarantee_small \
-                else banner_regular_expansion[self.args['random_pool'][self.args['pnt_random_pool']]]
+                else expansion_regular_banner[self.args['random_pool'][self.args['pnt_random_pool']]]
             self.args['pnt_random_pool'] += 1
 
             if result >= -1:
@@ -183,33 +202,81 @@ class Gacha_Expansion:
 
 
 # %%
-x_truncate = 200
-y_focus_truncate, y_expansion_truncate = [0] * x_truncate, [0] * x_truncate
+class Gacha_New:
+    def __init__(self, args):
+        self.args = args
+        self.pity_counter_small = 0
+        self.pity_counter_big = 0
+        self.equipment = np.array([0, 0, 0, 0])
+        self.total_pulling = 0
+
+    def single_pull(self):
+        if self.pity_counter_big >= guarantee_big:
+            self.pity_counter_small = 0
+            self.pity_counter_big = 0
+            self.equipment[random.sample(list(np.where(self.equipment == 0)[0]), 1)[0]] += 1
+
+            return True
+        else:
+            result = guarantee_banner[self.args['random_pool'][self.args['pnt_random_pool']]] \
+                if self.pity_counter_small >= guarantee_small \
+                else new_regular_banner[self.args['random_pool'][self.args['pnt_random_pool']]]
+            self.args['pnt_random_pool'] += 1
+
+            if result >= -1:
+                self.pity_counter_small = 0
+
+                if result >= 0:
+                    if self.equipment[result] == 0:
+                        self.pity_counter_big = 0
+
+                    self.equipment[result] += 1
+                    return True
+
+            return False
+
+    def reset_pity_counter(self):
+        return True if \
+            (0 not in self.equipment or
+             (self.equipment[0] and np.count_nonzero(self.equipment) == 3 and
+              (self.equipment[1] + self.equipment[2] + self.equipment[3]) >= 4)) \
+            else False
+
+    def pull(self):
+        while True:
+            self.pity_counter_small += 1
+            self.pity_counter_big += 1
+            self.total_pulling += 1
+
+            if self.single_pull() and self.reset_pity_counter():
+                break
+
+        return [self.total_pulling, self.equipment]
 
 
+# %%
 def test():
-    y_focus_temp, y_expansion_temp = {}, {}
+    _y_focus_temp, _y_expansion_temp, _y_new_temp = np.array([0] * preserved_size), np.array([0] * preserved_size), np.array([0] * preserved_size)
 
     args = {
-        'random_pool': np.random.randint(0, banner_sample_size, gacha_sample_size * 300),
+        'random_pool': np.random.randint(0, sample_accuracy, banner_size),
         'pnt_random_pool': 0
     }
 
     for i in range(gacha_sample_size):
         result_focus = Gacha_Focus(args).pull()[0]
         result_expansion = Gacha_Expansion(args).pull()[0]
-        if result_focus < x_truncate:
-            if result_focus not in y_focus_temp:
-                y_focus_temp.update({result_focus: 1})
-            else:
-                y_focus_temp[result_focus] += 1
-        if result_expansion < x_truncate:
-            if result_expansion not in y_expansion_temp:
-                y_expansion_temp.update({result_expansion: 1})
-            else:
-                y_expansion_temp[result_expansion] += 1
+        result_new = Gacha_New(args).pull()[0]
 
-    return [y_focus_temp, y_expansion_temp]
+        _y_focus_temp[result_focus] += 1
+        _y_expansion_temp[result_expansion] += 1
+        _y_new_temp[result_new] += 1
+
+        if args['pnt_random_pool'] >= pnt_threshold:
+            np.random.shuffle(args['random_pool'])
+            args['pnt_random_pool'] = 0
+
+    return [_y_focus_temp, _y_expansion_temp, _y_new_temp]
 
 
 if __name__ == '__main__':
@@ -224,23 +291,22 @@ if __name__ == '__main__':
             processing_pool.apply_async(test)
         ])
 
+    y_focus, y_expansion, y_new = np.array([0] * preserved_size), np.array([0] * preserved_size), np.array([0] * preserved_size)
     for gacha_result in gacha_results:
-        [y_focus, y_expansion] = gacha_result[1].get()
-        for k in y_focus.keys():
-            y_focus_truncate[k] += y_focus[k]
-        for k in y_expansion.keys():
-            y_expansion_truncate[k] += y_expansion[k]
+        [y_focus_temp, y_expansion_temp, y_new_temp] = gacha_result[1].get()
+        y_focus += y_focus_temp
+        y_expansion += y_expansion_temp
+        y_new += y_new_temp
         print('第{}个进程计算完毕'.format(gacha_result[0] + 1))
 
     processing_pool.close()
     print('总共耗时{}s'.format(int(time.time() - start_time)))
 
-    # %%
     def draw_result():
         data = [
             go.Scatter(
                 x=[i for i in range(x_truncate)],
-                y=y_focus_truncate[:x_truncate],
+                y=y_focus[:x_truncate],
                 mode='markers',
                 marker={
                     'color': 'rgb(255, 0, 0)'
@@ -249,12 +315,21 @@ if __name__ == '__main__':
             ),
             go.Scatter(
                 x=[i for i in range(x_truncate)],
-                y=y_expansion_truncate[:x_truncate],
+                y=y_expansion[:x_truncate],
                 mode='markers',
                 marker={
                     'color': 'rgb(0, 0, 255)'
                 },
                 name='expansion'
+            ),
+            go.Scatter(
+                x=[i for i in range(x_truncate)],
+                y=y_new[:x_truncate],
+                mode='markers',
+                marker={
+                    'color': 'rgb(0, 255, 0)'
+                },
+                name='new'
             ),
         ]
         fig = go.Figure(
@@ -265,32 +340,32 @@ if __name__ == '__main__':
 
     draw_result()
 
-    # %%
     def generate_statistics():
-        average_focus, average_expansion = 0, 0
-        sum_focus, sum_expansion = sum(y_focus_truncate), sum(y_expansion_truncate)
+        print('精准补给平均数: {}'.format(np.average(np.array(range(len(y_focus))), weights=y_focus)))
+        print('扩充补给平均数: {}'.format(np.average(np.array(range(len(y_expansion))), weights=y_expansion)))
+        print('新补给池平均数: {}'.format(np.average(np.array(range(len(y_new))), weights=y_new)))
+
+        temp_num_focus, temp_num_expansion, temp_num_new = sum(y_focus) / 2.0, sum(y_expansion) / 2.0, sum(y_new) / 2.0
 
         for i in range(x_truncate):
-            average_focus += i * y_focus_truncate[i]
-            average_expansion += i * y_expansion_truncate[i]
-
-        print('精准补给平均数: {}'.format(average_focus / sum_focus))
-        print('扩充补给平均数: {}'.format(average_expansion / sum_expansion))
-
-        temp_sum_focus, temp_sum_expansion = sum_focus / 2.0, sum_expansion / 2.0
-
-        for i in range(x_truncate):
-            if temp_sum_focus >= y_focus_truncate[i]:
-                temp_sum_focus -= y_focus_truncate[i]
+            if temp_num_focus >= y_focus[i]:
+                temp_num_focus -= y_focus[i]
             else:
                 print('精准补给中位数: {}'.format(i))
                 break
 
         for i in range(x_truncate):
-            if temp_sum_expansion >= y_expansion_truncate[i]:
-                temp_sum_expansion -= y_expansion_truncate[i]
+            if temp_num_expansion >= y_expansion[i]:
+                temp_num_expansion -= y_expansion[i]
             else:
                 print('扩充补给中位数: {}'.format(i))
+                break
+
+        for i in range(x_truncate):
+            if temp_num_new >= y_new[i]:
+                temp_num_new -= y_new[i]
+            else:
+                print('新补给池中位数: {}'.format(i))
                 break
 
     generate_statistics()
